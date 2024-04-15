5139173d9849651fe196d762da27ac2a318d13dd
zStrikke <zstrikke@gmail.com>
2024-04-15 18:22:14 +0200

    funny script

---
+++ where-am-i.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo -e "\033[40m############################################\033[m"
+echo -e "URL: \033[4mhttps://zforum.zstrikke.local/\033[m"
+echo -e "\033[40m############################################\033[m"
+printf "[INFO] Processing info project: bip bup beep boop\033[0K\r"
+sleep 4
+printf "[INFO] Showing info\033[0K\r\n"
+echo -e "PHP: \033[42m8.2.18\033[m"
+echo -e "Composer: \033[42m2.6.5\033[m"
+echo -e "MySQL: \033[42m8.0.33\033[m"
+echo -e "NGINX: \033[42m1.25.1\033[m"
+echo -e "REDIS: \033[42m7.0.11\033[m"
+echo -e "Laravel: \033[42m10.48.7\033[m"
+echo "--------------------------------------------"
+echo -e "\033[44mHappy Coding!\033[m"
